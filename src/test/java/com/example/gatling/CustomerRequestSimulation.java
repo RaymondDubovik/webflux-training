fb2627a541160b89a5421dff2fffd7a407c9691a
Dubovik <raimonds.duboviks@ndgit.com>
2022-04-18 21:37:34 +0200

    Gatling setup

---
+++ src/test/java/com/example/gatling/CustomerRequestSimulation.java
@@ -0,0 +1,31 @@
+package com.example.gatling;
+
+import java.time.Duration;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class CustomerRequestSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:8080")
+            .acceptHeader("application/json")
+            .userAgentHeader("Gatling Performance Test");
+
+    ScenarioBuilder scn = scenario("Load Test Getting List")
+            .exec(http("get-customer-request")
+                          .get("/")
+                          .check(status().is(200))
+                 );
+
+    public CustomerRequestSimulation() {
+        this.setUp(scn.injectOpen(constantUsersPerSec(50).during(Duration.ofSeconds(15))))
+                .protocols(httpProtocol);
+    }
+}
